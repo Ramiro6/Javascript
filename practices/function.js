b6038fba7749c9c8fffcdb4a15b27729d6ac0328
Ramiro <ramiroruiz578@gmail.com>
2016-12-02 14:01:16 +0100

    first

---
+++ practices/function.js
@@ -0,0 +1,18 @@
+////////
+function sum(one,two) {
+  return one + two;
+}
+
+var i = sum(5,5);
+
+console.log(i);
+
+////
+
+function multiply(one,two) {
+  return one * two
+}
+
+var x = multiply(2,2);
+
+console.log(x);
---
+++ practices/practices.js
@@ -0,0 +1,79 @@
+// arrays
+//
+// var array = [10,20,30,40,50];
+//
+// for (i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+//
+// loops
+//
+// for (i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+//
+// for (i = 0; i >= -10; i--) {
+//   console.log(i);
+// }
+//
+// var x = 0
+// while (x < 10) {
+//   x++
+//   console.log(x);
+// }
+//
+// var x = 0
+// while (x > -10) {
+//   x--
+//   console.log(x);
+// }
+//
+// booleans
+//
+// var x = true
+// var y = false
+//
+// if (x === true ) {
+//   console.log("ITS OK");
+// }
+//
+//
+// conditionals
+//
+// var x = 10
+//
+// if (x > 11) {
+//   console.log("Is older");
+// }
+// else if (x === 11) {
+//   console.log("=");
+// }
+// else {
+//   console.log("Is less");
+// }
+
+// var array = [5,5,5,5]
+//
+// var total = array.reduce (function(sum,num){
+//   return sum + num
+// })
+//
+// console.log(total);
+//
+// var array = ["bread","jam","ice cream"]
+//
+// var one_Filter = array.filter(function(food){
+//   return food !== "bread"
+// })
+//
+// console.log(one_Filter);
+
+//object
+
+// var animal = {
+//   dog: "Guauuuuuu",
+//   num: 4,
+//   style: "pug"
+// }
+//
+// console.log(animal.dog);
