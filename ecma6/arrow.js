8567b4dd57e67aae7bd1f5a712f85952a13162cd
Ramiro <ramiroruiz578@gmail.com>
2017-02-05 13:46:45 +0100

    first

---
+++ ecma6/arrow.js
@@ -0,0 +1,43 @@
+// Arrow function
+
+var numero = [1,2,3,4,5]
+
+var multiplo = numero.map(function(n) {
+  return n * n;
+});
+
+console.log(multiplo);
+
+////////
+var numero = [1,2,3,4,5]
+
+var multiplo = numero.map( (n) => {
+  return n * n;
+});
+
+console.log(multiplo);
+
+////////
+
+setTimeout(function() {
+  this.nombre = "pepe"
+  console.log(this.nombre);
+},500);
+
+setTimeout(function() {
+  this.nombre = "joselo"
+  console.log(this.nombre);
+},1000)
+
+////////
+
+
+setTimeout(() => {
+  this.nombre = "pepe"
+  console.log(this.nombre);
+},500);
+
+setTimeout(() => {
+  this.nombre = "joselo"
+  console.log(this.nombre);
+},1000)
