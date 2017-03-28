79dab5c01d5c707532a7c7f53d1d50f99e2c54b1
Ramiro6 <fresco_esp@hotmail.com>
2017-03-28 12:17:50 +0200

    first

---
+++ practices/class-name-array.js
@@ -0,0 +1,16 @@
+class Name {
+  constructor(first) {
+    this.first = first
+  }
+
+  printAllName() {
+    for( var i = 0; i < this.first.length; i++) {
+      console.log(this.first[i]);
+    }
+  }
+
+}
+
+var x = new Name(["jose", "pepe", "manolo"])
+
+x.printAllName()
