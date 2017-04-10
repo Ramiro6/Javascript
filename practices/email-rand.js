246f0537e957918ed089ad85cf87fc875ee9196a
Ramiro6 <fresco_esp@hotmail.com>
2017-04-10 20:05:50 +0200

    first

---
+++ practices/email-rand.js
@@ -0,0 +1,17 @@
+class Email {
+  constructor(email) {
+    this.email = email
+  }
+
+  rr () {
+    console.log(this.email);
+  }
+  
+}
+
+var puts = new Email("ramiro@gmail.com")
+
+
+
+
+puts.rr()
