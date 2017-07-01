73f2a5b24da810d0c62f95f5762c913facebbf58
Ramiro6 <fresco_esp@hotmail.com>
2017-07-02 00:15:36 +0200

    practice

--- typescript/practice/card.ts
+++ typescript/practice/card.ts
@@ -3,16 +3,17 @@ import { db } from './db'
 
 class Player {
   database:number = 0
-  compu1:number = 0
-  player:number = 0
 
   constructor(data){
     this.database = data
     let compu1:number = data[Math.round(Math.floor(Math.random() * (11 - 0)) + 0)]
     let player:number = data[Math.round(Math.floor(Math.random() * (11 - 0)) + 0)]
-    // console.log(data[Math.round(Math.floor(Math.random() * (11 - 0)) + 0)])
 
-    // console.log(player)
+    if(compu1 > player){
+      console.log("You Win")
+    } else {
+      console.log("You loses")
+    }
   }
 
   wild() {
@@ -21,16 +22,4 @@ class Player {
 
 }
 
-class Compu extends Player {
-
-
-}
-
-
-// console.log(db[Math.round(Math.floor(Math.random() * (11 - 0)) + 0)])
-
-let Play = new Player(db)
-// let Pc = new Compu(db)
-//
-
-// let resutaldo = Play > Pc
+let play = new Player(db)
