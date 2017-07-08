b1229ec97c29a9d94693f0c8ff95aabc4fddb46c
Ramiro6 <fresco_esp@hotmail.com>
2017-07-08 17:19:20 +0200

    append

--- ecma6/paginator/paginator.js
+++ ecma6/paginator/paginator.js
@@ -1,12 +1,50 @@
-$( document ).ready(()=> {
-  const db = [{
+const DB = [{
+    id: 1,
     name: "Ramiro",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  }]
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+  {
+    id: 2,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 3,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 4,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 5,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 6,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 7,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+  {
+    id: 8,
+    name: "another",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicingelit."
+  },
+]
 
+
+$( document ).ready(()=> {
   let find = () => {
-    for( let i of db) {
-      $(".pp").html(i.name)
+    for( let i of DB) {
+      $(".pp").append(`<ul>${i.id}</ul>`)
 
       console.log(i);
     }
